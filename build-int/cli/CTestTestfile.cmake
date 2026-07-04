59df5d1d333582b7d8112c5dfa49495eaec7a6de
jmossymoss <jordan.moss@live.co.uk>
2026-07-04 20:38:55 +0000

    Untrack the local build-int directory; ignore build*/
    A git add -A during the branch integration swept CMake scratch from a
    side build directory into the tree. Remove it from tracking and widen
    the ignore pattern so any build*/ variant stays out.

--- build-int/cli/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Weft/cli
-# Build directory: /home/user/Weft/build-int/cli
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build-int/core/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/user/Weft/core
-# Build directory: /home/user/Weft/build-int/core
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
